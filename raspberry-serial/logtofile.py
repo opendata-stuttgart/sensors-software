a9c921c52e14f34aff95b909740f612fcb7ba8eb
Andreas Madsack <andreas@madflex.de>
2015-04-11 19:48:29 +0200

    remove extension

--- raspberry-serial/logtofile.py
+++ raspberry-serial/logtofile.py
@@ -18,7 +18,7 @@ dirname = os.path.dirname(os.path.abspath(__file__))
 # different filename on each restart
 # because you may have to change the location of the sensor
 # before uploading the data
-filename = os.path.join(dirname, uuid.uuid4().hex + '.data')
+filename = os.path.join(dirname, uuid.uuid4().hex)
 
 ser = serial.Serial('/dev/ttyACM0')
 while True:
