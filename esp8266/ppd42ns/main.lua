8ae3487b01a1c77c88f6a100298de15c0b15dc76
Andreas Madsack <andreas@madflex.de>
2015-05-25 17:13:35 +0200

    code cleanup

--- esp8266/ppd42ns/main.lua
+++ esp8266/ppd42ns/main.lua
@@ -1,28 +1,24 @@
 
-pin = 4  -- gpio2
+pin=4  -- gpio2
 
-sampletime=15000; -- 15 seconds
-durationP1=0 ;
+sampletime=15000 -- 15 seconds
+durationP1=0
 
-trigOnP1=0;
-lowpulseoccupancyP1=0;
+trigOnP1=0
+lowpulseoccupancyP1=0
 
 gpio.mode(pin, gpio.INT, gpio.PULLUP)
 
 function pin4change(level)
 
    if level == gpio.LOW then
---  and trigP1 == false
---      trigP1 = true;
-      trigOnP1 = tmr.now();
+      trigOnP1 = tmr.now()
       gpio.trig(pin, "up")
    end
    
    if level == gpio.HIGH then
--- and trigP1 == true
-      durationP1 = tmr.now() - trigOnP1;
-      lowpulseoccupancyP1 = lowpulseoccupancyP1 + durationP1;
---      trigP1 = false;
+      durationP1 = tmr.now() - trigOnP1
+      lowpulseoccupancyP1 = lowpulseoccupancyP1 + durationP1
       gpio.trig(pin, "down")
    end
 end
@@ -30,8 +26,8 @@ gpio.trig(pin, "down", pin4change)
 
 
 tmr.alarm(0,sampletime,1,function()
-	     ratio = lowpulseoccupancyP1 / (sampletime*10.0);
-	     print(lowpulseoccupancyP1);
-	     print(ratio);
-	     lowpulseoccupancyP1 = 0;
+	     ratio = lowpulseoccupancyP1 / (sampletime*10.0)
+	     print(lowpulseoccupancyP1)
+	     print(ratio)
+	     lowpulseoccupancyP1 = 0
 end)
