a721718b829251173a84d2151ec9fcd47d32ca48
Martin Weis <Martin.Weis@hft-stuttgart.de>
2015-07-06 18:08:15 +0200

    added new plots removed trailing spaces

--- r-scripts/luft.R
+++ r-scripts/luft.R
@@ -18,11 +18,11 @@ installrequire(c("xlsx","lubridate","dplyr","ggplot2"))
 #load(".RData")
 
 #Daten von Andreas laden
-# get data from 
+# get data from
 # https://paste.madflex.de/d9rcQxUT#particlesdata.csv
-# and clean errors: 
+# and clean errors:
 # sed -i -e '/\r.\+/d;/ovf/d;' particlesdata.csv
-# 
+#
 rdata.filename<-"dat.RData"
 if(file.exists(rdata.filename)){
     load(rdata.filename)
@@ -38,33 +38,33 @@ if(file.exists(rdata.filename)){
     }
 
     #ppd <- filter(dat, type=="PPD42NS")
-    # 
+    #
     ## Timestamps formatieren
     # pattern <- "[0-9]+-[0-9]+-[0-9]+ [0-9][0-9]:[0-9][0-9]:[0-9][0-9]"
-    # dat <- dat %>% 
-    #     mutate(timestamp2 = regexpr(pattern, timestamp)) %>% 
-    #     mutate(timestamp2 = regmatches(timestamp, timestamp2)) %>% 
+    # dat <- dat %>%
+    #     mutate(timestamp2 = regexpr(pattern, timestamp)) %>%
+    #     mutate(timestamp2 = regmatches(timestamp, timestamp2)) %>%
     #     mutate(timestamp2=ymd_hms(timestamp2))
     dat$timestamp2<-as.POSIXct(strptime(dat$timestamp,format="%Y-%m-%d %H:%M:%S"))
     # dat$timestamp2<-strptime(dat$timestamp,format="%d.%m.%y %H:%M:%S+%Z")
 
     # str(dat)
-    # 'data.frame':   319836 obs. of  27 variables:                                                                                                               
-    #  $ timestamp        : Factor w/ 319831 levels "2015-04-05 23:13:10.759068+00:00",..: 319822 319805 319788 319771 319754 319737 319720 319703 319686 319669 ...                                                                                                                                                          
+    # 'data.frame':   319836 obs. of  27 variables:
+    #  $ timestamp        : Factor w/ 319831 levels "2015-04-05 23:13:10.759068+00:00",..: 319822 319805 319788 319771 319754 319737 319720 319703 319686 319669 ...
     # sensor type
-    #  $ type             : Factor w/ 3 levels "dsm501a","GP2Y1010AU0F",..: 1 1 1 1 1 1 1 1 1 1 ...                                                               
-    #  $ indoor           : Factor w/ 2 levels "False","True": 2 2 2 2 2 2 2 2 2 2 ...                                                                            
-    #  $ location_id      : int  4 4 4 4 4 4 4 4 4 4 ...                                                                                                          
-    #  $ sampling_rate    : int  15000 15000 15000 15000 15000 15000 15000 15000 15000 15000 ...                                                                  
-    # 
-    #  $ P1               : Factor w/ 61894 levels "","017.95","05.06",..: 1 1 1 1 1 1 1 1 1 1 ...                                                                
-    #  $ P2               : num  NA NA NA NA NA NA NA NA NA NA ...                                                                                                
-    #  $ durP1            : num  NA NA NA NA NA NA NA NA NA NA ...                                                                                                
-    #  $ durP2            : int  NA NA NA NA NA NA NA NA NA NA ...                                                                                                
-    #  $ ratioP1          : num  NA NA NA NA NA NA NA NA NA NA ...                                                                                                
-    #  $ ratioP2          : num  NA NA NA NA NA NA NA NA NA NA ...                                                                                                
-    #  $ temperature      : logi  NA NA NA NA NA NA ...                                                                                                           
-    #  $ humidity         : logi  NA NA NA NA NA NA ...                                                                                                           
+    #  $ type             : Factor w/ 3 levels "dsm501a","GP2Y1010AU0F",..: 1 1 1 1 1 1 1 1 1 1 ...
+    #  $ indoor           : Factor w/ 2 levels "False","True": 2 2 2 2 2 2 2 2 2 2 ...
+    #  $ location_id      : int  4 4 4 4 4 4 4 4 4 4 ...
+    #  $ sampling_rate    : int  15000 15000 15000 15000 15000 15000 15000 15000 15000 15000 ...
+    #
+    #  $ P1               : Factor w/ 61894 levels "","017.95","05.06",..: 1 1 1 1 1 1 1 1 1 1 ...
+    #  $ P2               : num  NA NA NA NA NA NA NA NA NA NA ...
+    #  $ durP1            : num  NA NA NA NA NA NA NA NA NA NA ...
+    #  $ durP2            : int  NA NA NA NA NA NA NA NA NA NA ...
+    #  $ ratioP1          : num  NA NA NA NA NA NA NA NA NA NA ...
+    #  $ ratioP2          : num  NA NA NA NA NA NA NA NA NA NA ...
+    #  $ temperature      : logi  NA NA NA NA NA NA ...
+    #  $ humidity         : logi  NA NA NA NA NA NA ...
     #  $ pressure         : logi  NA NA NA NA NA NA ...
     #  $ altitude         : logi  NA NA NA NA NA NA ...
     #  $ pressure_sealevel: logi  NA NA NA NA NA NA ...
@@ -82,7 +82,7 @@ if(file.exists(rdata.filename)){
 
     # P1 should be numeric
     # TODO: identify problematic entries
-    # DONE with preprocessing (sed) 
+    # DONE with preprocessing (sed)
 #     dat$P1num<-as.numeric(as.character(dat$P1))
 #     ## dust_density should be numeric
 #     dat$dust_density_num=as.numeric(as.character(dat$dust_density))
@@ -90,10 +90,12 @@ if(file.exists(rdata.filename)){
 } #end if else read data
 
 ## Plots, to pdf
-pdf("plots.pdf")
+# pdf("plots.pdf")
+png(width=1200,height=1000)
+
 #Werte fuer PM2.5 im Zeitverlauf
 p1 <- ggplot(filter(dat, type=="PPD42NS"), aes(timestamp2, P2, color=type)) + geom_point() + theme(axis.text.x = element_text(angle=90)) + geom_point(data=filter(dat, type == "GP2Y1010AU0F"), aes(timestamp2, P25, color=type)) + scale_y_log10()
-#+ geom_point(data=filter(dat, type=="GP2Y1010AU0F"), aes(timestamp2, vo_raw, color=type)) 
+#+ geom_point(data=filter(dat, type=="GP2Y1010AU0F"), aes(timestamp2, vo_raw, color=type))
 print(p1)
 
 # density plots
@@ -121,7 +123,7 @@ p1<-ggplot(dat, aes(x = ratioP2)) + stat_density() + labs(title="ratioP2 density
 print(p1)
 
 p1<-ggplot(dat, aes(x = ratioP2)) + stat_density() + labs(title="ratioP2 density") + xlim(c(0,15))
-#+ scale_x_log10() 
+#+ scale_x_log10()
 print(p1)
 
 p1<-ggplot(dat, aes(x = durP1)) + geom_density(color="red",trim=TRUE) +xlim(c(0,3446407))+ labs(title="durP1/durP2 densities (red/blue)") + geom_density(aes(x = durP2),color="blue",trim=TRUE)
@@ -149,11 +151,45 @@ p1<-ggplot(dat, aes(x = P25)) + geom_density(aes(x = durP2),color="black",trim=T
 print(p1)
 
 
-p1<-ggplot(dat, aes(x = dust_density)) + geom_bar()+ labs(title="dust_density histogram") + xlim(c(-0.01,0.08)); print(p1) 
+p1<-ggplot(dat, aes(x = dust_density)) + geom_bar()+ labs(title="dust_density histogram") + xlim(c(-0.01,0.08)); print(p1)
 
 p1<-ggplot(dat, aes(x = P10)) + stat_density() + labs(title="P10 density/sensor type") + xlim(c(8000,30000))+ facet_wrap(~type)
 print(p1)
 
+
+p1 <- ggplot(filter(dat, type=="PPD42NS"), aes(P1, P2)) + geom_point() +scale_x_log10() + scale_y_log10() + facet_wrap(~location_id)
+print(p1)
+
+p1 <- ggplot(filter(dat, type=="PPD42NS"), aes(P1, P2,color=as.double(timestamp2))) + scale_colour_continuous() + geom_point() +scale_x_log10() + scale_y_log10() + facet_wrap(~location_id)
+print(p1)
+
+p1 <- ggplot(dat, aes(P25, P10)) + geom_point() +scale_x_log10() + scale_y_log10() + facet_wrap(type~location_id, drop=TRUE,shrink=TRUE)
+print(p1)
+
+p1 <- ggplot(dat, aes(P1, P2)) + geom_point() +scale_x_log10() + scale_y_log10() + facet_wrap(type~location_id, drop=TRUE,shrink=TRUE)
+print(p1)
+
+p1 <- ggplot(dat, aes(ratioP1, ratioP2)) + geom_point() + facet_wrap(type~location_id, drop=TRUE,shrink=TRUE)  +scale_x_log10() + scale_y_log10()
+print(p1)
+
+p1 <- ggplot(dat, aes(durP1, durP2)) + geom_point() + facet_wrap(type~location_id, drop=TRUE,shrink=TRUE) #+scale_x_log10() + scale_y_log10()
+print(p1)
+
+p1 <- ggplot(dat, aes(vo_raw, voltage)) + geom_point() + facet_wrap(type~location_id, drop=TRUE,shrink=TRUE)  #+scale_x_log10() + scale_y_log10()
+print(p1)
+
+p1 <- ggplot(dat, aes(timestamp2, P1)) + geom_point() + facet_wrap(type~location_id, drop=TRUE,shrink=TRUE)  + scale_y_log10()
+print(p1)
+
+p1 <- ggplot(dat) + geom_line(aes(timestamp2, P1)) + geom_line(aes(timestamp2, P2, color="red")) + facet_wrap(type~location_id,ncol=1, drop=TRUE,shrink=TRUE)  + scale_y_log10()
+print(p1)
+
+# measurement times
+p1 <- ggplot(dat) + geom_density(aes(x = timestamp2),color="blue",trim=TRUE,kernel="rectangular",width=0.1) + labs(title="Measurement density")
+print(p1)
+
+p1<- ggplot(dat) + geom_point(aes(timestamp2,P2,order = sample(seq_along(timestamp2))))
+print(p1)
 
 dev.off() # end plot to pdf
 
@@ -175,4 +211,3 @@ table(dat$type,as.POSIXlt(dat$timestamp2)$yday,useNA = "ifany")
 table(dat$type,as.POSIXlt(dat$timestamp2)$yday,sapply(dat$P2,is.na),useNA = "no")
 # how many NA's in P1?
 table(dat$type,as.POSIXlt(dat$timestamp2)$yday,sapply(dat$P1,is.na),useNA = "no")
-
