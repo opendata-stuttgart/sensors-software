fcacc786f850af9ae71b953da017e93c40d9a793
Arne Schauf <git@as-webservices.de>
2015-07-21 13:26:25 +0200

    use nodemcu 0.9.6 included dht reader

--- esp8266/plugin_framework/dht_reader.lua
+++ esp8266/plugin_framework/dht_reader.lua
@@ -1,18 +1,15 @@
 local config = require('config')
 
 local drf_api = require('drf_api')
-local DHT = require("dht_lib")
 
 local function timerCB()
-    DHT.read22(4) -- GPIO2
-    t = DHT.getTemperature()
-    h = DHT.getHumidity()
-    if t == nil or h == nil then
-        print("Error reading from DHT11/22")
-    else
-        t = t * 0.1
-        h = h * 0.1
-        drf_api.send_dht22_to_api(t, h)
+    status,temp,humi,temp_decimial,humi_decimial = dht.readxx(config.PIN_DHT)
+    if( status == dht.OK ) then
+        drf_api.send_dht22_to_api(temp, humi)
+    elseif( status == dht.ERROR_CHECKSUM ) then
+        print( "DHT Checksum error." );
+    elseif( status == dht.ERROR_TIMEOUT ) then
+        print( "DHT Time out." );
     end
 end
 
