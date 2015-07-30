1bed20d4549551c5b17520e13b99c8f95b0980c2
Andreas Madsack <andreas@madflex.de>
2015-07-31 01:08:18 +0200

    added filter for 10ms between triggers. but the results are not really improving. :(

--- esp8266/ppd42ns/main.lua
+++ esp8266/ppd42ns/main.lua
@@ -3,61 +3,72 @@ pin_P1=6  -- gpio12
 pin_P2=5  -- gpio14
 
 sampletime=30000 -- 30 seconds
+filter_time=10000 -- in us
 durationP1=0
+lastHighP1=0
 trigOnP1=0
 lowpulseoccupancyP1=0
+last_P1 = gpio.HIGH
 
 durationP2=0
+lastHighP2=0
 trigOnP2=0
 lowpulseoccupancyP2=0
+last_P2 = gpio.HIGH
 
 
 PIN_GREEN_LED = 2
 PIN_RED_LED = 1
 gpio.mode(PIN_GREEN_LED, gpio.OUTPUT)
-gpio.mode(PIN_RED_LED, gpio.OUTPUT)
 gpio.write(PIN_GREEN_LED, gpio.HIGH)
 c = gpio.HIGH
 
 
 gpio.mode(pin_P1, gpio.INT, gpio.PULLUP)
 function pin4change(level)
-
-   if level == gpio.LOW then
-      trigOnP1 = tmr.now()
-      gpio.trig(pin_P1, "up")
---      gpio.write(PIN_GREEN_LED, gpio.HIGH)
+   if (level == gpio.LOW) and (last_P1 == gpio.HIGH) then
+      if (tmr.now() - trigOnP1) > filter_time then
+	 last_P1 = gpio.LOW
+	 trigOnP1 = tmr.now()
+      end
    end
    
-   if level == gpio.HIGH then
-      durationP1 = tmr.now() - trigOnP1
-      lowpulseoccupancyP1 = lowpulseoccupancyP1 + durationP1
-      gpio.trig(pin_P1, "down")
---      gpio.write(PIN_GREEN_LED, gpio.LOW)
+   if (level == gpio.HIGH) and (last_P1 == gpio.LOW) then
+      if (tmr.now() - lastHighP1) > filter_time then
+	 last_P1 = gpio.HIGH
+	 lastHighP1 = tmr.now()
+	 durationP1 = lastHighP1 - trigOnP1
+	 lowpulseoccupancyP1 = lowpulseoccupancyP1 + durationP1
+      end
    end
+   gpio.trig(pin_P1, "both")
 end
-gpio.trig(pin_P1, "down", pin4change)
+gpio.trig(pin_P1, "both", pin4change)
 
 
 gpio.mode(pin_P2, gpio.INT, gpio.PULLUP)
 function pin2change(level)
-
-   if level == gpio.LOW then
-      trigOnP2 = tmr.now()
-      gpio.trig(pin_P2, "up")
+   if (level == gpio.LOW) and (last_P2 == gpio.HIGH) then
+      if (tmr.now() - trigOnP2) > filter_time then
+	 last_P2 = gpio.LOW
+	 trigOnP2 = tmr.now()
+      end
    end
    
-   if level == gpio.HIGH then
-      durationP2 = tmr.now() - trigOnP2
-      lowpulseoccupancyP2 = lowpulseoccupancyP2 + durationP2
-      gpio.trig(pin_P2, "down")
+   if (level == gpio.HIGH) and (last_P2 == gpio.LOW) then
+      if (tmr.now() - lastHighP2) > filter_time then
+	 last_P2 = gpio.HIGH
+	 lastHighP2 = tmr.now()
+	 durationP2 = lastHighP2 - trigOnP2
+	 lowpulseoccupancyP2 = lowpulseoccupancyP2 + durationP2
+      end
    end
+   gpio.trig(pin_P2, "both")
 end
-gpio.trig(pin_P2, "down", pin2change)
+gpio.trig(pin_P2, "both", pin2change)
 
 
 tmr.alarm(0,sampletime,1,function()
-
     if wifi.sta.getip() == nil then
         status = wifi.sta.status()
         if status == 1 then -- connecting
@@ -67,19 +78,13 @@ tmr.alarm(0,sampletime,1,function()
 	   else
 	      c = gpio.HIGH
 	   end
-	   gpio.write(PIN_RED_LED, c)
         elseif status <= 4 then
 	   gpio.write(PIN_GREEN_LED, gpio.LOW)
-	   gpio.write(PIN_RED_LED, gpio.HIGH)
         elseif status == 5 then
 	   gpio.write(PIN_GREEN_LED, gpio.HIGH)
-	   gpio.write(PIN_RED_LED, gpio.LOW)
         end
     else
         print('IP: ',wifi.sta.getip())
-        gpio.write(PIN_RED_LED, gpio.LOW)
-        pwm.setup(PIN_GREEN_LED, 100, 10)
-        pwm.start(PIN_GREEN_LED)
 	send_to_api(lowpulseoccupancyP1, lowpulseoccupancyP2, sampletime)
     end
     -- even if the data could not be sent.
