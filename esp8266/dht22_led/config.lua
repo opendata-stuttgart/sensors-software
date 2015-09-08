5b6d9bf21a942216d0fddec9ca641ae5508ddfde
Martin Weis <Martin.Weis@hft-stuttgart.de>
2015-09-08 17:41:10 +0200

    test lua project for some sensors

---
+++ esp8266/dht22_led/config.lua
@@ -0,0 +1,14 @@
+return {
+    WIFI_SSID = 'shack',
+    WIFI_KEY = 'welcome2shack',
+
+    PLUGINS = {
+        'dht_reader',
+        -- 'reedswitch',
+    },
+
+    PIN_DHT=3,
+    PIN_REED_SWITCH = 5,  -- GPIO 14
+    PIN_GREEN_LED = 2, 
+    PIN_RED_LED = 1
+}
