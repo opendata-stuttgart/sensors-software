03a336e25bf432700f771e39cae872522405595b
Andreas Madsack <andreas@madflex.de>
2015-10-18 22:44:02 +0200

    sending data to API now works. data is always 0. needs further debugging.

--- esp8266-arduino/ppd42ns-wifi-dht/ppd42ns-wifi-dht.ino
+++ esp8266-arduino/ppd42ns-wifi-dht/ppd42ns-wifi-dht.ino
@@ -1,28 +1,28 @@
-/*******************************************************/
-/* OK LAB Particulate Matter Sensor                    */
-/*      - nodemcu-LoLin board                          */
-/*      - Shinyei PPD42NS                              */
-/*      http://www.sca-shinyei.com/pdf/PPD42NS.pdf     */
-/*                                                     */       
-/* Wiring Instruction:                                 */
-/*      Pin 2 of dust sensor PM2.5 -> Digital 6 (PWM)  */
-/*      Pin 3 of dust sensor       -> +5V              */
-/*      Pin 4 of dust sensor PM1   -> Digital 3 (PMW)  */ 
-/*                                                     */
-/*      - PPD42NS Pin 1 (grey)  => GND                 */
-/*      - PPD42NS Pin 2 (green) => Pin D5 /GPIO14      */
-/*        counts particles PM25                        */
-/*      - PPD42NS Pin 3 (black) => Vin                 */
-/*      - PPD42NS Pin 4 (white) => Pin D6 / GPIO12     */
-/*        counts particles PM10                        */
-/*      - PPD42NS Pin 5 (red)   => unused              */
-/*                                                     */
-/*******************************************************/
+/************************************************************/
+/* OK LAB Particulate Matter Sensor                         */
+/*      - nodemcu-LoLin board                               */
+/*      - Shinyei PPD42NS                                   */
+/*      http://www.sca-shinyei.com/pdf/PPD42NS.pdf          */
+/*                                                          */       
+/* Wiring Instruction:                                      */
+/*      Pin 2 of dust sensor PM2.5 -> Digital 6 (PWM)       */
+/*      Pin 3 of dust sensor       -> +5V                   */
+/*      Pin 4 of dust sensor PM1   -> Digital 3 (PMW)       */ 
+/*                                                          */
+/*      - PPD42NS Pin 1 (grey or green)  => GND             */
+/*      - PPD42NS Pin 2 (green or white)) => Pin D5 /GPIO14 */
+/*        counts particles PM25                             */
+/*      - PPD42NS Pin 3 (black or yellow) => Vin            */
+/*      - PPD42NS Pin 4 (white or black) => Pin D6 / GPIO12 */
+/*        counts particles PM10                             */
+/*      - PPD42NS Pin 5 (red)   => unused                   */
+/*                                                          */
+/************************************************************/
 
 /**********************************************/
 /* DHT declaration 
 /**********************************************/
-#include <DHT.h>
+#include "DHT.h"
 #define DHTPIN 4
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
@@ -32,9 +32,12 @@ DHT dht(DHTPIN, DHTTYPE);
 /**********************************************/
 #include <ESP8266WiFi.h>
 
-const char* ssid = "*****";
-const char* password = "*****";
+const char* ssid = ".....";
+const char* password = ".....";
+// const char* host = "192.168.1.19";
 const char* host = "api.dusti.xyz";
+// const int httpPort = 8000;
+const int httpPort = 80;
 int value = 0;
 
 /**********************************************/
@@ -53,7 +56,7 @@ boolean trigP2 = false;
 unsigned long trigOnP1;
 unsigned long trigOnP2;
 
-unsigned long sampletime_ms = 15000;
+unsigned long sampletime_ms = 30000;
 unsigned long lowpulseoccupancyP1 = 0;
 unsigned long lowpulseoccupancyP2 = 0;
 
@@ -69,9 +72,9 @@ void setup() {
   pinMode(12,INPUT); // Listen at the designated PIN
   pinMode(14,INPUT); //Listen at the designated PIN
   starttime = millis(); // store the start time
-  dht.begin(); // Start DHT
+//  dht.begin(); // Start DHT
   delay(10);
-  // connectWifi(); // Start ConnecWifi 
+  connectWifi(); // Start ConnecWifi 
   Serial.print("\n"); 
 }
 
@@ -79,6 +82,8 @@ void setup() {
 /* And action
 /**********************************************/
 void loop() {
+  String data;
+
   // Read pins connected to ppd42ns
   valP1 = digitalRead(12);
   valP2 = digitalRead(14);
@@ -112,68 +117,81 @@ void loop() {
     concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // spec sheet curve
     // Begin printing
     Serial.print("LPO P10     : ");
-    Serial.print(lowpulseoccupancyP1);
-    Serial.print("\n");
+    Serial.println(lowpulseoccupancyP1);
     Serial.print("Ratio PM10  : ");
     Serial.print(ratio);
-    Serial.print(" %");
-    Serial.print("\n");
+    Serial.println(" %");
     Serial.print("PM10 Count  : ");
-    Serial.print(concentration);
-    Serial.println("\n");
+    Serial.println(concentration);
+
+    // json for push to api / P1
+    data = "{\"sensordatavalues\":[{";
+    data += "\"value_type\":\"durP1\",\"value\":\"";
+    data += Float2String(lowpulseoccupancyP1);
+    data += "\"},{";
+    data += "\"value_type\":\"ratioP1\",\"value\":\"";
+    data += Float2String(ratio);
+    data += "\"},{";
+    data += "\"value_type\":\"P1\",\"value\":\"";
+    data += Float2String(concentration);
+    data += "\"},{";
 
     ratio = lowpulseoccupancyP2/(sampletime_ms*10.0);
     concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
     // Begin printing
     Serial.print("LPO PM25    : ");
-    Serial.print(lowpulseoccupancyP2);
-    Serial.print("\n");
+    Serial.println(lowpulseoccupancyP2);
     Serial.print("Ratio PM25  : ");
     Serial.print(ratio);
-    Serial.print(" %");
-    Serial.print("\n");
+    Serial.println(" %");
     Serial.print("PM25 Count  : ");
-    Serial.print(concentration);
-    Serial.println("\n");
+    Serial.println(concentration);
+
+    // json for push to api / P2
+    data += "\"value_type\":\"durP2\",\"value\":\"";
+    data += Float2String(lowpulseoccupancyP1);
+    data += "\"},{";
+    data += "\"value_type\":\"ratioP2\",\"value\":\"";
+    data += Float2String(ratio);
+    data += "\"},{";
+    data += "\"value_type\":\"P2\",\"value\":\"";
+    data += Float2String(concentration);
+    data += "\"}]}";
+
+    //sending to dustix api
+    Serial.println("#### Sending to dusti.xyz: ");
+    sendData(data);
     
     // Resetting for next sampling
     lowpulseoccupancyP1 = 0;
     lowpulseoccupancyP2 = 0;
     starttime = millis(); // store the start time
 
-    //connectiong to dusty api
-    // Serial.println("#### Sending to Dusty: ");
-    // connect2API();
-    // Serial.print("\n");
-
+    // FIXME: option to send PIN
     // sensorDHT();  // getting temperature and humidity (optional)
-    // Serial.print("------------------------------");
-    // Serial.print("\n");
-
+    Serial.println("------------------------------");
   }
 }
-/**********************************************/
-/* DHT22 Sensor
-/**********************************************/
+// DHT22 Sensor
 void sensorDHT(){
-  float h = dht.readHumidity(); //Read Humidity
-  float t = dht.readTemperature(); //Read Temperature
+ float h = dht.readHumidity(); //Read Humidity
+ float t = dht.readTemperature(); //Read Temperature
 
-  // Check if valid number if non NaN (not a number) will be send.
-  if (isnan(t) || isnan(h)) {
-    Serial.println("DHT22 couldn’t be read");
-  } else {
-    Serial.print("Humidity    : ");
-    Serial.print(h);
-    Serial.print(" %\n");
-    Serial.print("Temperature : ");
-    Serial.print(t);
-    Serial.println(" C");
-    
-    //Serial.println("#### Sending to Dusty: ");
-    //connect2API();
-    //Serial.print("\n");
-  }
+ // Check if valid number if non NaN (not a number) will be send.
+ if (isnan(t) || isnan(h)) {
+   Serial.println("DHT22 couldn’t be read");
+ } else {
+   Serial.print("Humidity    : ");
+   Serial.print(h);
+   Serial.print(" %\n");
+   Serial.print("Temperature : ");
+   Serial.print(t);
+   Serial.println(" C");
+   
+   //Serial.println("#### Sending to Dusty: ");
+   //connect2API();
+   //Serial.print("\n");
+ }
 }
 
 /**********************************************/
@@ -190,14 +208,13 @@ void connectWifi() {
   }
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
-  Serial.print(WiFi.localIP());
-  Serial.print('\n');
+  Serial.println(WiFi.localIP());
 }
 /**********************************************/
-/* Connect 2 API Script
+/* send data to rest api
 /**********************************************/
-void connect2API() {
-  delay(60000);
+void sendData(const String& data) {
+  //  delay(60000);
   ++value;
 
   Serial.print("connecting to ");
@@ -205,7 +222,6 @@ void connect2API() {
   
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
-  const int httpPort = 8000;
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
     return;
@@ -217,6 +233,7 @@ void connect2API() {
   Serial.print("Requesting URL: ");
   Serial.println(url);
   Serial.println(ESP.getChipId());
+  Serial.println(data);
   
   // send request to the server
   client.print(String("POST ") + url + " HTTP/1.1\r\n" +
@@ -224,7 +241,12 @@ void connect2API() {
                "Content-Type: application/json\r\n" +
                "Sensor: esp8266-");
   client.println(ESP.getChipId());
+  client.print("Content-Length: ");
+  client.println(data.length(), DEC);
   client.println("Connection: close\r\n");
+
+  client.println(data);
+
   delay(1);
   
   // Read reply from server and print them
@@ -235,4 +257,15 @@ void connect2API() {
   
   Serial.println();
   Serial.println("closing connection");
+}
+
+String Float2String(float value)
+{
+  // Convert a float to String with two decimals.
+  String s;
+  s = String(int(value));
+  s += '.';
+  s += int((value - int(value)) * 100);
+
+  return s;
 }
---
+++ esp8266-arduino/ppd42ns-wifi-dht/readme.txt
@@ -0,0 +1,4 @@
+
+
+
+source of DHT.h and DHT.cpp: https://github.com/adafruit/DHT-sensor-library
