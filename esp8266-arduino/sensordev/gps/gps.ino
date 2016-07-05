5c62216bdd1ad7c8d38f25199fbe19791f9972a7
Martin Weis <Martin.Weis.newsadress@gmx.de>
2016-07-05 16:54:00 +0200

    gps sketch to test

---
+++ esp8266-arduino/sensordev/gps/gps.ino
@@ -0,0 +1,76 @@
+/* config, move later to sensorconfig.h */
+
+#define PUSHTO_SERIAL
+// TODO implement
+// #define PUSHTO_MQTT
+
+// GPIO PINSs <-> nodemcu PINs (Dx) mapping
+//          0  1  2  3  4   5   6   7   8  9  10  11  12
+int D[13]={16, 5, 4, 0, 2, 14, 12, 13, 15, 3,  1,  9, 10};
+
+#define GPS_ACTIVE
+#ifdef GPS_ACTIVE
+/* config, move later to sensorconfig.h */
+// wiring: GPS6MV2-RX->GPSRXPin
+static const int GPSRXPin = D[7];
+static const int GPSTXPin = D[8];
+static const uint32_t GPSBaud = 9600;
+unsigned long gpsmaxtime_ms=2100;
+unsigned int pin_led = D[0];
+
+#define DEBUG_GPS_NMEA
+
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include "gpsfunctions.h"
+#endif //GPS_ACTIVE
+
+void blink_led(unsigned int pin,unsigned int n=2, unsigned int interval=100){
+unsigned int i=0;
+    digitalWrite(pin, LOW);
+    delay(interval);
+    for (i=0; i<n;i++){
+        digitalWrite(pin, HIGH);
+        delay(interval);
+        digitalWrite(pin, LOW);
+        delay(interval);
+    }
+}
+
+void setup(){
+#ifdef PUSHTO_SERIAL
+    Serial.begin(9600);
+    Serial.println("start");
+    Serial.print("chipid\t");
+    Serial.println(ESP.getChipId());
+#endif //PUSHTO_SERIAL
+
+    pinMode(pin_led, OUTPUT);
+    blink_led(pin_led,3,500);
+    
+#ifdef GPS_ACTIVE
+    gps_setup();
+#endif //GPS_ACTIVE
+
+    pinMode(pin_led, OUTPUT);
+    blink_led(pin_led,2,500);
+
+}
+
+void loop(){
+blink_led(pin_led,2,200);
+bool success=gps_read();
+if (!success){
+blink_led(pin_led,5,100);
+#ifdef PUSHTO_SERIAL
+    Serial.print(millis());
+    Serial.println("\tGPS failed");
+#endif //PUSHTO_SERIAL
+}else{
+    push_gps_location();
+    blink_led(pin_led,1,1000);
+    Serial.println("\tGPS read success");
+
+}    
+delay(10000);
+}
---
+++ esp8266-arduino/sensordev/gps/gpsfunctions.h
@@ -0,0 +1,169 @@
+#ifndef GPSFUNCTIONS_H
+#define GPSFUNCTIONS_H
+// include in .ino
+// #include <TinyGPS++.h>
+// get from http://arduiniana.org/libraries/tinygpsplus/
+// #include <SoftwareSerial.h>
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+// The serial connection to the GPS device
+SoftwareSerial ss(GPSRXPin, GPSTXPin);
+
+void gps_setup(){
+  ss.begin(GPSBaud);
+}
+
+// This custom version of delay() ensures that the gps object
+// is being "fed".
+bool feedDelayGPS(unsigned long ms=200)
+{
+  #ifdef DEBUG_GPS_NMEA
+  char recchar;
+  #endif
+  unsigned long start = millis();
+  do
+  {
+    while (ss.available()){
+      #ifdef DEBUG_GPS_NMEA
+      recchar=ss.read();
+      Serial.print(recchar);
+      gps.encode(recchar);
+      #endif
+      #ifndef DEBUG_GPS_NMEA
+      gps.encode(ss.read());
+      #endif
+    
+      if (gps.location.isUpdated()){
+          return true;
+      }
+    }
+  } while (millis() - start < ms);
+#ifdef DEBUG_GPS_NMEA
+    Serial.println("charsProcessed() sentencesWithFix() failedChecksum() passedChecksum()");
+    Serial.println(gps.charsProcessed());
+    Serial.println(gps.sentencesWithFix());
+    Serial.println(gps.failedChecksum());
+    Serial.println(gps.passedChecksum());
+#endif
+  return false;
+}
+
+bool push_gps_location(){
+    if (!gps.location.isValid()){
+        return false;   
+    }
+#ifdef PUSHTO_SERIAL
+Serial.print("LatLonAlt\t");
+Serial.print(gps.location.lat(),6);
+Serial.print("\t");
+Serial.print(gps.location.lng(),6);
+Serial.print("\t");
+Serial.println(gps.altitude.meters());
+#endif
+        #ifdef PUSHTO_MQTT
+        mqtt_publish_subtopic("GPS/lat",gps.location.lat());
+        mqtt_publish_subtopic("GPS/lon",gps.location.lng());
+        mqtt_publish_subtopic("GPS/height",gps.altitude.meters());
+        #endif
+}
+
+bool push_gps_datetime(){
+    TinyGPSDate d=gps.date;
+    TinyGPSTime t=gps.time;
+    char sz[64];
+    if (!d.isValid()){
+        return false;
+    }else{
+        sprintf(sz, "%02d-%02d-%02d", d.year(), d.month(), d.day());
+        #ifdef PUSHTO_SERIAL
+        Serial.print("DateTimeAge\t");
+        Serial.print(sz);
+        #endif
+        #ifdef PUSHTO_MQTT
+        mqtt_publish_subtopic("GPS/date",gps.hdop.value());
+        #endif
+    }
+    if (!t.isValid()){
+        Serial.println("");
+        return false;
+    }else{
+        sprintf(sz, "%02d:%02d:%02d", t.hour(), t.minute(), t.second(),gps.location.age());
+        #ifdef PUSHTO_SERIAL
+        Serial.print(" ");
+        Serial.println(sz);
+        #endif
+        #ifdef PUSHTO_MQTT
+        mqtt_publish_subtopic("GPS/time",sz);
+        #endif
+        sprintf(sz, "%02d", gps.location.age());
+        #ifdef PUSHTO_SERIAL
+        Serial.print("\t");
+        Serial.println(sz);
+        #endif
+        #ifdef PUSHTO_MQTT
+        mqtt_publish_subtopic("GPS/locationage",sz);
+        #endif
+    }
+    return true;
+}
+bool push_gps_info(){
+      bool retval = (gps.hdop.isValid() & gps.satellites.isValid());
+      #ifdef PUSHTO_SERIAL
+      Serial.print("HdopSat\t");
+      if(gps.hdop.isValid()){
+          Serial.print(gps.hdop.value());
+      }
+      Serial.print("\t");
+      if(gps.satellites.isValid()){
+          Serial.print(gps.satellites.value());
+      }
+      Serial.println("");
+      #endif
+#ifdef PUSHTO_MQTT    
+      if(gps.hdop.isValid()){
+        mqtt_publish_subtopic("GPS/HDOP",gps.hdop.value());
+      }
+      if(gps.satellites.isValid()){
+        mqtt_publish_subtopic("GPS/satellites",gps.satellites.value());
+      }
+#endif
+      return retval;
+}
+bool gps_read(){
+    bool success=false;
+    // ss.flush(); //only outgoing data
+    if(!feedDelayGPS(gpsmaxtime_ms)){
+        // no news from GPS
+        #ifdef PUSHTO_SERIAL
+        Serial.println("no news from GPS");
+        #endif
+        return false;
+    }
+//     if(gps.location.isUpdated()){
+//     unsigned long starttime=millis();
+//     while(millis()-gpsmaxtime_ms<starttime){
+//      if((ss.available() > 0)){
+//        if (gps.encode(ss.read())){
+//          if (gps.location.isUpdated()){
+            success=(success|push_gps_location());
+            success=(success|push_gps_datetime());
+            success=(success|push_gps_info());
+            return success;
+//         }
+//       }
+//      }
+//     }
+
+//     Serial.print("debug: starttime=");
+//     Serial.println (starttime);
+//     if(!success){
+//         #ifdef PUSHTO_SERIAL
+//         Serial.println("error reading GPS");
+//         #endif
+//         return false;
+//     }
+//     return success;
+}
+
+#endif // GPSFUNCTIONS_H
