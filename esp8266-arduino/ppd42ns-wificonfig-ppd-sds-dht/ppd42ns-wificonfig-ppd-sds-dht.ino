opendata-stuttgart/sensors-software

commit ea1b214bde406a6158ff2b826dfd9b25b11ec5ed

some minor changes

esp8266-arduino/ppd42ns-wificonfig-ppd-sds-dht/ppd42ns-wificonfig-ppd-sds-dht.ino
@@ -58,7 +58,7 @@
 /*                                                               *
 /*****************************************************************/
 // increment on change
-#define SOFTWARE_VERSION "NRZ-2016-048"
+#define SOFTWARE_VERSION "NRZ-2016-049"
 
 /*****************************************************************
 /* Global definitions (moved to ext_def.h)                       *
@@ -772,10 +772,7 @@ void webserver_config() {
 	page_content.replace("{id}",esp_chipid);
 	page_content.replace("{fw}",SOFTWARE_VERSION);
 	page_content += FPSTR(WEB_CONFIG_SCRIPT);
-	if ((!server.hasArg("submit")) || (server.arg("submit") != "Speichern")) {
-		page_content.replace("{t}",F("Konfiguration"));
-		page_content.replace("{id}",esp_chipid);
-		page_content.replace("{fw}",SOFTWARE_VERSION);
+	if ((!server.hasArg("submit")) || (server.arg("submit") != "Save")) {
 		page_content += F("<form method='POST' action='/config' style='width: 100%;'>");
 		page_content += F("<b>WLAN Daten</b><br/>");
 		if (WiFi.status() != WL_CONNECTED) {  // scan for wlan ssids
@@ -785,7 +782,7 @@ void webserver_config() {
 			debug_out(F("wifi networks found: "),DEBUG_MIN_INFO,0);
 			debug_out(String(n),DEBUG_MIN_INFO,1);
 			if (n == 0) {
-				page_content += F("<br/>Kein WLAN gefunden<br/>");
+				page_content += F("<br/>Keine Netzwerke gefunden<br/>");
 			} else {
 				page_content += F("<br/>Netzwerke gefunden: ");
 				page_content += String(n);
@@ -837,7 +834,7 @@ void webserver_config() {
 		page_content += F("<table>");
 		page_content += form_input(F("debug"),F("Debug&nbsp;Level"),String(debug),5);
 		page_content += F("</table><br/><b>Weitere APIs</b><br/><br/>");
-		page_content += form_checkbox(F("send2custom"),F("Senden an eigene API"),send2custom);
+		page_content += form_checkbox(F("send2custom"),F("An eigene API senden"),send2custom);
 		page_content += F("<table>");
 		page_content += form_input(F("host_custom"),F("Server: "),host_custom,50);
 		page_content += form_input(F("url_custom"),F("Pfad: "),url_custom,50);
@@ -981,12 +978,12 @@ void webserver_debug_level() {
 	page_content.replace("{fw}",SOFTWARE_VERSION);
 	if (server.hasArg("level")) {
 		switch (server.arg("level").toInt()) {
-			case (0): debug=0; page_content += F("<h3>Set debug to none.</h3>");break;
-			case (1): debug=1; page_content += F("<h3>Set debug to error.</h3>");break;
-			case (2): debug=2; page_content += F("<h3>Set debug to warning.</h3>");break;
-			case (3): debug=3; page_content += F("<h3>Set debug to min. info.</h3>");break;
-			case (4): debug=4; page_content += F("<h3>Set debug to med. info.</h3>");break;
-			case (5): debug=5; page_content += F("<h3>Set debug to max. info.</h3>");break;
+			case (0): debug=0; page_content += F("<h3>Setze Debug auf none.</h3>");break;
+			case (1): debug=1; page_content += F("<h3>Setze Debug auf error.</h3>");break;
+			case (2): debug=2; page_content += F("<h3>Setze Debug auf warning.</h3>");break;
+			case (3): debug=3; page_content += F("<h3>Setze Debug auf min. info.</h3>");break;
+			case (4): debug=4; page_content += F("<h3>Setze Debug auf med. info.</h3>");break;
+			case (5): debug=5; page_content += F("<h3>Setze Debug auf max. info.</h3>");break;
 		}
 	}
 	page_content += FPSTR(WEB_PAGE_FOOTER);
@@ -1001,7 +998,7 @@ void webserver_removeConfig() {
 	last_page_load = millis();
 	debug_out(F("output remove config page..."),DEBUG_MIN_INFO,1);
 	page_content += FPSTR(WEB_PAGE_HEADER);
-	page_content.replace("{t}","config.json löschen");
+	page_content.replace("{t}","Config.json löschen");
 	page_content.replace("{id}",esp_chipid);
 	page_content.replace("{fw}",SOFTWARE_VERSION);
 	if (server.hasArg("confirm") && server.arg("confirm") == "yes") {
@@ -1032,7 +1029,7 @@ void webserver_reset() {
 	last_page_load = millis();
 	debug_out(F("output reset NodeMCU page..."),DEBUG_MIN_INFO,1);
 	page_content += FPSTR(WEB_PAGE_HEADER);
-	page_content.replace("{t}","Reset NodeMCU");
+	page_content.replace("{t}","Sensor neu starten");
 	page_content.replace("{id}",esp_chipid);
 	page_content.replace("{fw}",SOFTWARE_VERSION);
 	if (server.hasArg("confirm") && server.arg("confirm") == "yes") {
@@ -1895,7 +1892,6 @@ void init_display() {
 void setup() {
 	Serial.begin(9600);					// Output to Serial at 9600 baud
 #if defined(ESP8266)
-	Wire.pins(D3,D4);					// must be set before all other I2C cmds 
 	Wire.begin(D3,D4);
 	esp_chipid = String(ESP.getChipId());
 #endif

esp8266-arduino/ppd42ns-wificonfig-ppd-sds-dht/html-content.h
@@ -20,11 +20,12 @@ input[type='password']{width:100%;}\
 <h3 style='min-height:100px;'><a href='/' style='background:none;width:0;'><img src='/luftdaten_logo.svg' style='float:left;margin-right:20px'/></a>{t} Feinstaubsensor {id}<br/><small>Firmware {fw}</small></h3>\
 ";
 
-const char WEB_PAGE_FOOTER[] PROGMEM = "<br/><br/><a href='http://codefor.de/' target='_blank' style='display:inline;background:none;width:0px;'><img src='/cfg_logo.svg' style='width:200px;max-width:50%;'/></a>\
+const char WEB_PAGE_FOOTER[] PROGMEM = "<br/><br/><a href='/' style='display:inline;'>Zurück zur Startseite</a><br/><br/><br/>\
+<a href='http://codefor.de/' target='_blank' style='display:inline;background:none;width:0px;'><img src='/cfg_logo.svg' style='width:200px;max-width:50%;'/></a>\
 </body></html>\r\n";
 
-const char WEB_ROOT_PAGE_CONTENT[] PROGMEM = "<a href='/values'>aktuelle Werte</a><br/>\
-<a href='http://opendata-stuttgart.github.io/feinstaub-map/' target='_blank'>Karte der aktiven Sensoren</a><br/>\
+const char WEB_ROOT_PAGE_CONTENT[] PROGMEM = "<a href='/values'>Aktuelle Werte</a><br/>\
+<a href='http://opendata-stuttgart.github.io/feinstaub-map/' target='_blank'>Karte der aktiven Sensoren (externer Link)</a><br/>\
 <a href='/config'>Konfiguration</a><br/>\
 <a href='/removeConfig'>Konfiguration löschen</a><br/>\
 <a href='/reset'>Sensor neu starten (Reset)</a><br/>\
@@ -46,7 +47,7 @@ const char WEB_REMOVE_CONFIG_CONTENT[] PROGMEM = "<h3>Konfiguration wirklich lö
 <table><tr><td><a href='/removeConfig?confirm=yes'>Löschen</a></td><td><a href='/'>Abbrechen</a></td></tr></table>\
 ";
 
-const char WEB_RESET_CONTENT[] PROGMEM = "<h3>Wirklich neu starten?</h3>\
+const char WEB_RESET_CONTENT[] PROGMEM = "<h3>Sensor wirklich neu starten?</h3>\
 <table><tr><td><a href='/reset?confirm=yes'>Neu starten</a></td><td><a href='/'>Abbrechen</a></td></tr></table>\
 ";
 

commit b2fb2296106501f08c7898cff27c80253f4aef11

Fix saving of configuration
Broken since ea1b214bde406a6158ff2b826dfd9b25b11ec5ed

The value of the submit button did not match the condition in the handler.
Checking the request method is better since there is only one form.
Even better would be routing based on the request method.

esp8266-arduino/ppd42ns-wificonfig-ppd-sds-dht/ppd42ns-wificonfig-ppd-sds-dht.ino
@@ -805,7 +805,7 @@ void webserver_config() {
 	page_content.replace("{mac}",WiFi.macAddress());
 	page_content.replace("{fw}",SOFTWARE_VERSION);
 	page_content += FPSTR(WEB_CONFIG_SCRIPT);
-	if ((!server.hasArg("submit")) || (server.arg("submit") != "Save")) {
+	if (server.method() == HTTP_GET) {
 		page_content += F("<form method='POST' action='/config' style='width: 100%;'>");
 		page_content += F("<b>WLAN Daten</b><br/>");
 		if (WiFi.status() != WL_CONNECTED) {  // scan for wlan ssids
@@ -851,7 +851,7 @@ void webserver_config() {
 		page_content += F("<table>");
 		page_content += form_input(F("wlanssid"),F("WLAN"),wlanssid,64);
 		page_content += form_password(F("wlanpwd"),F("Passwort"),wlanpwd,64);
-		page_content += F("</table><br/><input type='submit' name='submit' value='Speichern'/><br/><br/><b>BasicAuth</b><br/>");
+		page_content += F("</table><br/><input type='submit' value='Speichern'/><br/><br/><b>BasicAuth</b><br/>");
 		page_content += F("<table>");
 		page_content += form_input(F("www_username"),F("User"),www_username,64);
 		page_content += form_password(F("www_password"),F("Passwort"),www_password,64);
@@ -888,8 +888,8 @@ void webserver_config() {
 		page_content += form_input(F("user_influxdb"),F("Benutzer: "),user_influxdb,50);
 		page_content += form_input(F("pwd_influxdb"),F("Passwort: "),pwd_influxdb,50);
 		page_content += F("</table><br/>");
-		page_content += F("<br/><input type='submit' name='submit' value='Speichern'/></form>");
-	} else {
+		page_content += F("<br/><input type='submit' value='Speichern'/></form>");
+	} else if (server.method() == HTTP_POST) {
 		if (server.hasArg("wlanssid") && server.arg("wlanssid") != "") {
 			server.arg("wlanssid").toCharArray(wlanssid,65);
 			if (server.hasArg("wlanpwd") && server.arg("wlanpwd") != "") {
@@ -952,6 +952,8 @@ void webserver_config() {
 		page_content += F("<br/>Passwort "); page_content += pwd_influxdb;
 		page_content += F("<br/><br/><a href='/reset?confirm=yes'>Gerät neu starten?</a>");
 
+	} else {
+		server.send(405, FPSTR(TXT_CONTENT_TYPE_TEXT_PLAIN), "Method not allowed");
 	}
 	page_content += FPSTR(WEB_PAGE_FOOTER);
 	server.send(200,FPSTR(TXT_CONTENT_TYPE_TEXT_HTML),page_content);