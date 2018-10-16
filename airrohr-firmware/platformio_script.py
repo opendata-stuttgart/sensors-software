287623148397deb7e39b54a4f0b62350ed434bd1
Rajko Zschiegner <ricki_z@gmx.de>
2018-10-16 22:07:21 +0200

    platformio extra script: disable float compile flags

--- airrohr-firmware/platformio_script.py
+++ airrohr-firmware/platformio_script.py
@@ -4,6 +4,14 @@ import os
 import shutil
 from base64 import b64decode
 
+flags = " ".join(env['LINKFLAGS'])
+flags = flags.replace("-u _printf_float", "")
+flags = flags.replace("-u _scanf_float", "")
+newflags = flags.split()
+env.Replace(
+  LINKFLAGS=newflags
+) 
+
 def after_build(source, target, env):
     shutil.copy(target[0].path, "./builds/latest_"+b64decode(ARGUMENTS.get("LANG"))+".bin")
 
