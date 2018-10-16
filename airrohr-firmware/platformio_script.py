8a622f3a0419b8d2e9bb18e72d11638ae7df6d06
Frank Meerkoetter <frank@meerkoetter.org>
2018-10-16 21:12:59 +0200

    Ensure that the target dir is there
    The build processing script assumed a directory to be there.
    If it wasn't, hitting the compile button in platformio would error.

--- airrohr-firmware/platformio_script.py
+++ airrohr-firmware/platformio_script.py
@@ -1,11 +1,11 @@
-
 Import("env")
 import os
 import shutil
 from base64 import b64decode
 
 def after_build(source, target, env):
+    if not os.path.exists("./builds"):
+        os.mkdir("./builds")
     shutil.copy(target[0].path, "./builds/latest_"+b64decode(ARGUMENTS.get("LANG"))+".bin")
-
 
 env.AddPostAction("$BUILD_DIR/firmware.bin", after_build)
