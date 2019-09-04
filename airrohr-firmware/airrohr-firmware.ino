f84765cae96d02e2eeb995df449a2e4383763c09
Rajko Zschiegner <ricki_z@gmx.de>
2019-09-04 08:59:20 +0200

    renaming emptyString
    emptyString was added to Arduino core for ESP8266 in version 2.5, but versions >= 2.5 generate a firmware image that is too large for OTA

--- airrohr-firmware/airrohr-firmware.ino
+++ airrohr-firmware/airrohr-firmware.ino
@@ -349,7 +349,7 @@ namespace cfg {
 
 #define JSON_BUFFER_SIZE 2300
 
-String emptyString;
+String empty_String;
 
 enum class PmSensorCmd {
 	Start,
@@ -1229,7 +1229,7 @@ String make_header(const String& title) {
 	if(title != " ") {
 		s.replace("{n}", F("&raquo;"));
 	} else {
-		s.replace("{n}", emptyString);
+		s.replace("{n}", empty_String);
 	}
 	s.replace("{t}", title);
 	s.replace("{id}", esp_chipid);
@@ -1284,12 +1284,12 @@ String form_checkbox(const char* name, const String& info, const bool checked, c
 	if (checked) {
 		s.replace("{c}", F(" checked='checked'"));
 	} else {
-		s.replace("{c}", emptyString);
+		s.replace("{c}", empty_String);
 	};
 	s.replace("{i}", info);
 	s.replace("{n}", name);
 	if (! linebreak) {
-		s.replace("<br/>", emptyString);
+		s.replace("<br/>", empty_String);
 	}
 	return s;
 }
@@ -1454,7 +1454,7 @@ static bool webserver_request_auth() {
 
 static void sendHttpRedirect() {
 	server.sendHeader(F("Location"), F("http://192.168.4.1/config"));
-	server.send(302, FPSTR(TXT_CONTENT_TYPE_TEXT_HTML), emptyString);
+	server.send(302, FPSTR(TXT_CONTENT_TYPE_TEXT_HTML), empty_String);
 }
 
 /*****************************************************************
@@ -2022,8 +2022,8 @@ void webserver_values() {
 			page_content += table_row_from_value(FPSTR(WEB_GPS), FPSTR(INTL_LATITUDE), check_display_value(last_value_GPS_lat, -200.0, 6, 0), "°");
 			page_content += table_row_from_value(FPSTR(WEB_GPS), FPSTR(INTL_LONGITUDE), check_display_value(last_value_GPS_lon, -200.0, 6, 0), "°");
 			page_content += table_row_from_value(FPSTR(WEB_GPS), FPSTR(INTL_ALTITUDE), check_display_value(last_value_GPS_alt, -1000.0, 2, 0), "m");
-			page_content += table_row_from_value(FPSTR(WEB_GPS), FPSTR(INTL_DATE), last_value_GPS_date, emptyString);
-			page_content += table_row_from_value(FPSTR(WEB_GPS), FPSTR(INTL_TIME), last_value_GPS_time, emptyString);
+			page_content += table_row_from_value(FPSTR(WEB_GPS), FPSTR(INTL_DATE), last_value_GPS_date, empty_String);
+			page_content += table_row_from_value(FPSTR(WEB_GPS), FPSTR(INTL_TIME), last_value_GPS_time, empty_String);
 		}
 
 		page_content += FPSTR(EMPTY_ROW);
@@ -2566,7 +2566,7 @@ void sendLuftdaten(const String& data, const int pin, const char* host, const in
 	String data_4_dusti = tmpl(FPSTR(data_first_part), SOFTWARE_VERSION);
 	data_4_dusti += data;
 	data_4_dusti.remove(data_4_dusti.length() - 1);
-	data_4_dusti.replace(replace_str, emptyString);
+	data_4_dusti.replace(replace_str, empty_String);
 	data_4_dusti += "]}";
 	if (data != "") {
 		sendData(data_4_dusti, pin, host, httpPort, url, use_ssl, verify, "", FPSTR(TXT_CONTENT_TYPE_JSON));
@@ -3882,9 +3882,9 @@ void display_values() {
 		}
 		if (cfg::has_lcd2004_27) {
 			display_header = String((next_display_count % screen_count) + 1) + "/" + String(screen_count) + " " + display_header;
-			display_lines[0].replace(" µg/m³", emptyString);
+			display_lines[0].replace(" µg/m³", empty_String);
 			display_lines[0].replace("°", String(char(223)));
-			display_lines[1].replace(" µg/m³", emptyString);
+			display_lines[1].replace(" µg/m³", empty_String);
 			lcd_2004_27.clear();
 			lcd_2004_27.setCursor(0, 0);
 			lcd_2004_27.print(display_header);
