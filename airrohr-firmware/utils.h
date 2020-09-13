50ea9c7dd97e2de41d79098be5124b2dd74d0739
Dirk Mueller <dirk@dmllr.de>
2020-09-13 21:07:11 +0200

    Fix formatting / code style
    We try to use tabs consistently. also use const String& instead of
    copy-by value string.

--- airrohr-firmware/utils.h
+++ airrohr-firmware/utils.h
@@ -147,7 +147,7 @@ extern void NPM_cmd(PmSensorCmd2 cmd);
 extern bool NPM_checksum_valid_4(const uint8_t (&data)[4]);
 extern bool NPM_checksum_valid_16(const uint8_t (&data)[16]);
 
-extern bool isNumeric(String str);
+extern bool isNumeric(const String& str);
 
 extern const __FlashStringHelper* loggerDescription(unsigned i);
 
