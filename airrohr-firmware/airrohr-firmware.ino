5fbf307e142f56f80b27c0490744dc05e56bce3f
Bas Stottelaar <basstottelaar@gmail.com>
2022-01-17 22:55:56 +0100

    Config page: group advance settings
    Settings such as debug level, update interval and router mode duration
    deserve their own section.

--- airrohr-firmware/airrohr-firmware.ino
+++ airrohr-firmware/airrohr-firmware.ino
@@ -1530,6 +1530,11 @@ static void webserver_config_send_body_get(String &page_content)
 					  "}; updateOTAOptions(); $('auto_update').onchange = updateOTAOptions;"
 					  "</script>");
 
+	page_content += FPSTR(WEB_BR_LF_B);
+	page_content += FPSTR(INTL_AB_HIER_NUR_ANDERN);
+	page_content += FPSTR(WEB_B_BR);
+	page_content += FPSTR(BR_TAG);
+
 	page_content += FPSTR(TABLE_TAG_OPEN);
 	add_form_input(page_content, Config_debug, FPSTR(INTL_DEBUG_LEVEL), 1);
 	add_form_input(page_content, Config_sending_intervall_ms, FPSTR(INTL_MEASUREMENT_INTERVAL), 5);
