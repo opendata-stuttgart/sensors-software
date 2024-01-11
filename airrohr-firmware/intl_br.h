b45d62deafcb60e41b7212a64a1973edda2a6185
Bruno Oliveira <84549919+xanymorex@users.noreply.github.com>
2024-01-11 00:54:18 -0300

    Create intl_br.h
    Including translation to PT-BR

---
+++ airrohr-firmware/intl_br.h
@@ -0,0 +1,132 @@
+/*
+ *	airRohr firmware
+ *	Copyright (C) 2016-2018  Code for Stuttgart a.o.
+ *
+ *  Translation template
+ *
+ *	Texts should be as short as possible
+ */
+
+#define INTL_LANG "BR";
+#define INTL_PM_SENSOR ""
+const char INTL_CONFIGURATION[] PROGMEM = "";
+#define INTL_WIFI_SETTINGS "Configuracões de Wi-Fi"
+#define INTL_WIFI_NETWORKS "Carregando redes Wi-Fi"
+#define INTL_LANGUAGE "Idioma"
+const char INTL_NO_NETWORKS[] PROGMEM =  "Redes Wi-Fi não detectadas";
+const char INTL_NETWORKS_FOUND[] PROGMEM = "Redes encontradas";
+const char INTL_AB_HIER_NUR_ANDERN[] PROGMEM = "Configuracões avançadas (apenas se sabe o que está fazendo)";
+const char INTL_SAVE[] PROGMEM = "Salvar";
+const char INTL_SENSORS[] PROGMEM = "Sensores";
+const char INTL_MORE_SENSORS[] PROGMEM = "Outros Sensores";
+const char INTL_SDS011[] PROGMEM = "SDS011 ({pm})";
+const char INTL_PMS[] PROGMEM = "Plantower PMS(1,3,5,6,7)003 ({pm})";
+const char INTL_HPM[] PROGMEM = "Honeywell PM ({pm})";
+const char INTL_NPM[] PROGMEM = "Tera Sensor Next PM ({pm})";
+const char INTL_NPM_FULLTIME[] PROGMEM = "Next PM fulltime";
+const char INTL_IPS[] PROGMEM = "Piera Systems IPS-7100 ({pm})";
+const char INTL_SPS30[] PROGMEM = "Sensirion SPS30 ({pm})";
+const char INTL_PPD42NS[] PROGMEM = "PPD42NS ({pm})";
+const char INTL_DHT22[] PROGMEM = "DHT22 ({t}, {h})";
+const char INTL_HTU21D[] PROGMEM = "HTU21D ({t}, {h})";
+const char INTL_BMP180[] PROGMEM = "BMP180 ({t}, {p})";
+const char INTL_BMX280[] PROGMEM = "BME280 ({t}, {h}, {p}), BMP280 ({t}, {p})";
+const char INTL_SHT3X[] PROGMEM = "SHT3X ({t}, {h})";
+const char INTL_SCD30[] PROGMEM = "SCD30 ({t}, {h}, CO₂)";
+const char INTL_DS18B20[] PROGMEM = "DS18B20 ({t})";
+const char INTL_DNMS[] PROGMEM = "DNMS ({l_a})";
+const char INTL_DNMS_CORRECTION[] PROGMEM = "Valor de correção em dB(A)";
+const char INTL_TEMP_CORRECTION[] PROGMEM = "Correção em C°";
+const char INTL_HEIGHT_ABOVE_SEALEVEL[] PROGMEM = "";
+const char INTL_PRESSURE_AT_SEALEVEL[] PROGMEM = "";
+const char INTL_NEO6M[] PROGMEM = "GPS (NEO 6M)";
+const char INTL_BASICAUTH[] PROGMEM = "Autorização";
+#define INTL_REPORT_ISSUE " Reporter um problema"
+
+const char INTL_FS_WIFI_DESCRIPTION[] PROGMEM = "Nome do sensor Wi-Fi em modo de configuração";
+const char INTL_FS_WIFI_NAME[] PROGMEM = "Nome";
+const char INTL_MORE_SETTINGS[] PROGMEM = "Outras configurações";
+const char INTL_AUTO_UPDATE[] PROGMEM = "Atualização automática de Firmware";
+const char INTL_USE_BETA[] PROGMEM = "Carregar Firmware Beta";
+const char INTL_DISPLAY[] PROGMEM = "OLED SSD1306";
+const char INTL_SH1106[] PROGMEM = "OLED SH1106";
+const char INTL_FLIP_DISPLAY[] PROGMEM = "Girar  a tela OLED";
+const char INTL_LCD1602_27[] PROGMEM = "LCD 1602 (I2C: 0x27)";
+const char INTL_LCD1602_3F[] PROGMEM = "LCD 1602 (I2C: 0x3F)";
+const char INTL_LCD2004_27[] PROGMEM = "LCD 2004 (I2C: 0x27)";
+const char INTL_LCD2004_3F[] PROGMEM = "LCD 2004 (I2C: 0x3F)";
+const char INTL_DISPLAY_WIFI_INFO[] PROGMEM = "Mostrar informações da rede Wi-Fi";
+const char INTL_DISPLAY_DEVICE_INFO[] PROGMEM = "Mostrar informações do dispositivo";
+
+#define INTL_STATIC_IP_TEXT ""
+const char INTL_STATIC_IP[] PROGMEM = "";
+const char INTL_STATIC_SUBNET[] PROGMEM = "";
+const char INTL_STATIC_GATEWAY[] PROGMEM = "";
+const char INTL_STATIC_DNS[] PROGMEM = "";
+
+const char INTL_DEBUG_LEVEL[] PROGMEM = "";
+const char INTL_MEASUREMENT_INTERVAL[] PROGMEM = "Intervalo de medição";
+const char INTL_DURATION_ROUTER_MODE[] PROGMEM = "Duração ";
+const char INTL_POWERSAVE[] PROGMEM = "";
+const char INTL_MORE_APIS[] PROGMEM = "Outros API";
+const char INTL_SEND_TO_OWN_API[] PROGMEM = "Enviar para a própria API";
+const char INTL_SERVER[] PROGMEM = "Servidor";
+const char INTL_PATH[] PROGMEM = "Caminho";
+const char INTL_PORT[] PROGMEM = "Porta";
+const char INTL_USER[] PROGMEM = "Usuário";
+const char INTL_PASSWORD[] PROGMEM = "Senha";
+const char INTL_MEASUREMENT[] PROGMEM = "Medição";
+const char INTL_SEND_TO[] PROGMEM = "Enviar para";
+const char INTL_READ_FROM[] PROGMEM = "Ler";
+const char INTL_SENSOR_IS_REBOOTING[] PROGMEM = "Sensor sendo reiniciado";
+const char INTL_RESTART_DEVICE[] PROGMEM = "Reiniciar dispositivo";
+const char INTL_DELETE_CONFIG[] PROGMEM = "Apagar configuração atual";
+const char INTL_RESTART_SENSOR[] PROGMEM = "Reiniciar sensor";
+#define INTL_HOME "Início"
+#define INTL_BACK_TO_HOME "Voltar ao início"
+const char INTL_CURRENT_DATA[] PROGMEM = "Dados atuais";
+const char INTL_DEVICE_STATUS[] PROGMEM = "Status do dispositivo";
+#define INTL_ACTIVE_SENSORS_MAP "Mapa de sensores ativos"
+#define INTL_CONFIGURATION_DELETE "Apagar configuração"
+#define INTL_CONFIGURATION_REALLY_DELETE "Confirms que quer apagar a configuração"
+#define INTL_DELETE "Apagar"
+#define INTL_CANCEL "Cancelar"
+#define INTL_REALLY_RESTART_SENSOR "Confirma que quer reiniciar o sensor?"
+#define INTL_RESTART "Reiniciar"
+const char INTL_SAVE_AND_RESTART[] PROGMEM = "Salvar e reiniciar";
+#define INTL_FIRMWARE "Versão do Firmaware"
+const char INTL_DEBUG_SETTING_TO[] PROGMEM = "Ajustar configuração de Debug ";
+#define INTL_NONE " nenhum"
+#define INTL_ERROR "erro"
+#define INTL_WARNING "aviso"
+#define INTL_MIN_INFO "Informação minima"
+#define INTL_MED_INFO "Informação média"
+#define INTL_MAX_INFO "Informação maxima"
+#define INTL_CONFIG_DELETED "Configuração apagada"
+#define INTL_CONFIG_CAN_NOT_BE_DELETED "Configuração não pode ser apagada"
+#define INTL_CONFIG_NOT_FOUND "Configuração não encontrada"
+const char INTL_TIME_TO_FIRST_MEASUREMENT[] PROGMEM = "Tempo para a primeira medição";
+const char INTL_TIME_SINCE_LAST_MEASUREMENT[] PROGMEM = "Tempo desde a última medição";
+const char INTL_PARTICLES_PER_LITER[] PROGMEM = "Particulas/litro";
+const char INTL_PARTICULATE_MATTER[] PROGMEM = "Particulas finas";
+const char INTL_TEMPERATURE[] PROGMEM = "Temperatura";
+const char INTL_HUMIDITY[] PROGMEM = "Humidade";
+const char INTL_PRESSURE[] PROGMEM = "Pessão atmosférica";
+const char INTL_DEW_POINT[] PROGMEM = "";
+const char INTL_CO2_PPM[] PROGMEM = "ppm CO₂";
+const char INTL_LEQ_A[] PROGMEM = "LAeq";
+const char INTL_LA_MIN[] PROGMEM = "LA min";
+const char INTL_LA_MAX[] PROGMEM = "LA max";
+const char INTL_LATITUDE[] PROGMEM = "Latitude";
+const char INTL_LONGITUDE[] PROGMEM = "Longitude";
+const char INTL_ALTITUDE[] PROGMEM = "Altitude";
+const char INTL_TIME_UTC[] PROGMEM = "Hora (UTC)";
+const char INTL_SIGNAL_STRENGTH[] PROGMEM = "Intensidade do Sinal";
+const char INTL_SIGNAL_QUALITY[] PROGMEM = "Qualidade do Sinal";
+#define INTL_NUMBER_OF_MEASUREMENTS "Quantidade de medições"
+#define INTL_TIME_SENDING_MS "Duração de transmissão de medições"
+#define INTL_SENSOR "Sensor"
+#define INTL_PARAMETER "Parâmetro"
+#define INTL_VALUE "Valor"
+
+#include "./airrohr-logo-common.h"
